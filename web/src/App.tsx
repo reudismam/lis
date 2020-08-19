f8c37302deb880ca698e55a164009968f9c87a44
Renan <renan.nascimento.santana@hotmail.com>
2020-08-19 01:00:37 -0300

    criando-menu

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,24 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App() {  /*faltando onClick*/ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className = "navbar">
+      <header>
+        <a href="#" className = "principal-item">LIS</a>
+        <div className = "menu-toggle">
+          <nav className="nav-menu">
+            <ul className="nav-menu-list">
+              <li><a className = "list-item">Início</a></li>
+              <li><a className = "list-item">Sobre</a></li>
+              <li><a className = "list-item">Pesquisadores</a></li>
+              <li><a className = "list-item">Publicações</a></li>
+              <li><a className = "list-item">Projetos</a></li>
+              <li><a className = "list-item">Portifólio</a></li>
+              <li><a className = "list-item">Notícia</a></li>
+              <li><a className = "list-item">Editais</a></li>
+              <li><a className = "list-item">Parceiros</a></li>
+              <li><a className = "list-item">Contatos</a></li>
+            </ul>
+            <ul className = "itens-final">
+              <li><a className = "list-item">Sair</a></li>
+              <li><a className = "list-item"><span>Criar uma conta</span></a></li>
+            </ul>
+          </nav>
+        </div>
+    </header>
+
     </div>
   );
 }
