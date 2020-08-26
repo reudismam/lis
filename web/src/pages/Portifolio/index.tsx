12e58d96a0776a56170de663ede2e56125907f56
Fernando Dutra <fernandozek@hotmail.com>
2020-08-26 03:42:50 -0300

    CSS pagina Publicacoes e Portifolio

--- web/src/pages/Portifolio/index.tsx
+++ web/src/pages/Portifolio/index.tsx
@@ -3,6 +3,9 @@ import {gsap} from 'gsap';
 import PageDefault from '../DefaultPage';
 import './styles.css';
 import PortfolioImg from './img/portfolio.png'
+import imgItem from './img/item.png';
+import btn1 from './img/btn1.png';
+import btn2 from './img/btn2.png';
 
 export default function Portifolio() {
 
@@ -27,14 +30,86 @@ export default function Portifolio() {
     },)
 
     return (
-        <PageDefault> 
-            <div className="portfolio">
-            <div className="portfolio-top">
-                    <div className="portfolio-text" ref={Animation}>
-                        <h1>Veja alguns portfólios</h1><br/>
-                        <p>Essas são algumas criações que nos orgulham!</p>
-                    </div><br/>
-                    <img alt="portfolio imagem" className="img-portfolio" src={PortfolioImg} ref={Animation2}/>
+        <PageDefault>
+            <div> 
+                <div className="portfolio">
+                    <div className="portfolio-top">
+                        <div className="portfolio-text" ref={Animation}>
+                            <h1>Veja alguns portfólios</h1><br/>
+                            <p>Essas são algumas criações que nos orgulham!</p>
+                        </div><br/>
+                        <img alt="portfolio imagem" className="img-portfolio" src={PortfolioImg} ref={Animation2}/>
+                    </div>
+                </div>
+                <div className="boxL">
+                    <div className="img">
+                        <img src={imgItem}/>
+                    </div>
+                    <div className="item">
+                        <div className="boxtop">
+                            <h4 className="h4">Lorem Ipsum dolor</h4> <p className="epoch">00/00/2020 - 3 semanas atrás</p>
+                        </div>
+                        <div className="conteudo">
+                            <p className="post">Id cursus metus aliquam eleifend mi in nulla posuere sollicitudin. Ut pharetra sit amet aliquam id diam maecenas ultricies. Euismod quis viverra nibh cras pulvinar mattis. Amet nulla facilisi morbi tempus iaculis. Vestibulum mattis ullamcorentesque </p>
+                            <br/>
+                            <p className="post">Id cursus metus aliquam eleifend mi in nulla posuere sollicitudin. Ut pharetra sit amet aliquam id diam maecenas ultricies. Euismod quis viverra nibh cras pulvinar mattis. Amet nulla facilisi morbi tempus iaculis. Vestibulum mattis ullamcorentesque </p>
+                        </div>
+                        <button className="button">Acesse</button>
+                    </div>
+                </div>
+                <div className="line">
+                    <hr/>
+                    <p>Semana passada</p>
+                </div>
+                <div className="grid">
+                    <div className="grid-item">
+                        <img src={imgItem}/>
+                        <h4 className="grid-h4 h4">Lorem Ipsum dolor</h4> 
+                        <div className="grid-post">
+                            <p className="post">Id cursus metus aliquam eleifend mi in nulla posuere sollicitudin. Ut pharetra sit amet aliquam id diam maecenas ultricies. Euismod quis viverra nibh cras pulvinar mattis. Amet nulla facilisi morbi tempus iaculis. Vestibulum mattis ullamcorentesque </p>
+                        </div>
+                        <div className="grid-bottom">
+                            <button className="button-grid">Acesse</button><p className="grid-epoch">00/00/2020 - 3 semanas atrás</p>
+                        </div>
+                    </div>
+                    <div className="grid-item">
+                        <img src={imgItem}/>
+                        <h4 className="grid-h4 h4">Lorem Ipsum dolor</h4> 
+                        <div className="grid-post">
+                            <p className="post">Id cursus metus aliquam eleifend mi in nulla posuere sollicitudin. Ut pharetra sit amet aliquam id diam maecenas ultricies. Euismod quis viverra nibh cras pulvinar mattis. Amet nulla facilisi morbi tempus iaculis. Vestibulum mattis ullamcorentesque </p>
+                        </div>
+                        <div className="grid-bottom">
+                            <button className="button-grid">Acesse</button><p className="grid-epoch">00/00/2020 - 3 semanas atrás</p>
+                        </div>
+                    </div>
+                    <div className="grid-item">
+                        <img src={imgItem}/>
+                        <h4 className="grid-h4 h4">Lorem Ipsum dolor</h4> 
+                        <div className="grid-post">
+                            <p className="post">Id cursus metus aliquam eleifend mi in nulla posuere sollicitudin. Ut pharetra sit amet aliquam id diam maecenas ultricies. Euismod quis viverra nibh cras pulvinar mattis. Amet nulla facilisi morbi tempus iaculis. Vestibulum mattis ullamcorentesque </p>
+                        </div>
+                        <div className="grid-bottom">
+                            <button className="button-grid">Acesse</button><p className="grid-epoch">00/00/2020 - 3 semanas atrás</p>
+                        </div>
+                    </div>
+                    <div className="grid-item">
+                        <img src={imgItem}/>
+                        <h4 className="grid-h4 h4">Lorem Ipsum dolor</h4> 
+                        <div className="grid-post">
+                            <p className="post">Id cursus metus aliquam eleifend mi in nulla posuere sollicitudin. Ut pharetra sit amet aliquam id diam maecenas ultricies. Euismod quis viverra nibh cras pulvinar mattis. Amet nulla facilisi morbi tempus iaculis. Vestibulum mattis ullamcorentesque </p>
+                        </div>
+                        <div className="grid-bottom">
+                            <button className="button-grid">Acesse</button><p className="grid-epoch">00/00/2020 - 3 semanas atrás</p>
+                        </div>
+                    </div>
+
+                </div>
+                <div className="btn-navigation">
+                    <button className="btn-page btn-selected">1</button>
+                    <button className="btn-page">2</button>
+                    <button className="btn-page">3</button>
+                    <button className="btn-page" ><img src={btn1}/></button>
+                    <button className="btn-page"><img src={btn2}/></button>
                 </div>
             </div>
          </PageDefault>
--- web/src/pages/Publicacoes/index.tsx
+++ web/src/pages/Publicacoes/index.tsx
@@ -27,14 +27,37 @@ export default function Publicacoes() {
     },)
 
     return (
-        <PageDefault> 
-            <div className="publicacoes">
-                <div className="publicacoes-top">
-                    <div className="publicacoes-text" ref={Animation}>
-                        <h1>Aqui estão as publicações</h1><br/>
-                        <p>Veja suas atribuições no momento, deixe seus dados sempre atualizados!</p>
-                    </div><br/>
-                    <img alt="publicações imagem" className="img-publicacoes" src={PublicacoesImg} ref={Animation2}/>
+        <PageDefault>
+            <div> 
+                <div className="publicacoes">
+                    <div className="publicacoes-top">
+                        <div className="publicacoes-text" ref={Animation}>
+                            <h1>Aqui estão as publicações</h1><br/>
+                            <p>Veja suas atribuições no momento, deixe seus dados sempre atualizados!</p>
+                        </div><br/>
+                        <img alt="publicações imagem" className="img-publicacoes" src={PublicacoesImg} ref={Animation2}/>
+                    </div>
+                </div>
+                <div className="box">
+                    <h2 className="data">2020</h2>
+                    <p className="conteudo">
+                    SOUSA, R. R.; LEITE, F. T. ; GUIMARAES, A. O. ; OLIVEIRA, A. R. . Pré-Algoritmos ? Ações de Apoio à Melhoria do Ensino de Graduação. Brazilian Journal of Development, v. 6, p. 12635-12635, 2020.
+                    COSTA, R. R. ; SOUSA, R. R. . O Uso de Tutores de Programação Inteligentes na Produção de Feedback para Estudantes em Tarefas de Programação: Uma Revisão Sistemática da Literatura. Brazilian Journal of Development, v. 6, p. brazilianjourna-29496, 2020.
+                    </p>
+                </div>
+                <div className="box">
+                    <h2 className="data">2019</h2>
+                    <p className="conteudo">
+                    SOUSA, R. R.; LEITE, F. T. ; GUIMARAES, A. O. ; OLIVEIRA, A. R. . Pré-Algoritmos ? Ações de Apoio à Melhoria do Ensino de Graduação. Brazilian Journal of Development, v. 6, p. 12635-12635, 2020.
+                    COSTA, R. R. ; SOUSA, R. R. . O Uso de Tutores de Programação Inteligentes na Produção de Feedback para Estudantes em Tarefas de Programação: Uma Revisão Sistemática da Literatura. Brazilian Journal of Development, v. 6, p. brazilianjourna-29496, 2020.
+                    </p>
+                </div>
+                <div className="box">
+                    <h2 className="data">2018</h2>
+                    <p className="conteudo">
+                    SOUSA, R. R.; LEITE, F. T. ; GUIMARAES, A. O. ; OLIVEIRA, A. R. . Pré-Algoritmos ? Ações de Apoio à Melhoria do Ensino de Graduação. Brazilian Journal of Development, v. 6, p. 12635-12635, 2020.
+                    COSTA, R. R. ; SOUSA, R. R. . O Uso de Tutores de Programação Inteligentes na Produção de Feedback para Estudantes em Tarefas de Programação: Uma Revisão Sistemática da Literatura. Brazilian Journal of Development, v. 6, p. brazilianjourna-29496, 2020.
+                    </p>
                 </div>
             </div>
          </PageDefault>
