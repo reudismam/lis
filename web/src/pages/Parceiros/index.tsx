41d4218eb744231f3a3522cc6f8778a82d18ace2
Emanuel-Bruno <emanuelbruno2018vasc@gmail.com>
2020-10-03 00:59:00 -0300

    Atualizando Styled-Components-Page-Parceiros

--- web/src/pages/Parceiros/index.tsx
+++ web/src/pages/Parceiros/index.tsx
@@ -1,12 +1,63 @@
 import React from 'react';
 import PageDefault from '../DefaultPage';
 import ParceirosImg from '../../assets/images/Parceiros/parceiros.svg';
-import UFERSA from './img/ufersa.png';
-import PROEC from './img/proec.png';
-import SEBRAE from './img/sebrae.png';
-import ME from './img/me.png';
-import RNgov from './img/rngov.png';
-import './styles.css';
+import UFERSA from '../../assets/images/Parceiros/ufersa.png';
+import PROEC from '../../assets/images/Parceiros/proec.png';
+import SEBRAE from '../../assets/images/Parceiros/sebrae.png';
+import ME from '../../assets/images/Parceiros/me.png';
+import RNgov from '../../assets/images/Parceiros/rngov.png';
+import styled from 'styled-components';
+
+const ParceirosContainer = styled.div`
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    @media(min-width: 768px){
+        padding: 0 50px 20px 50px;
+    }
+
+    @media(min-width: 1280px){
+        padding: 0 100px 20px 100px;
+    }
+
+`
+const ParceirosMiddle = styled.div`
+    background: #F0F0F7;
+    width: 100%;
+    min-width: 280px;
+    height: 100%;
+    padding: 10px;    
+
+`
+const ParceirosMiddleContent = styled.div`
+    background: #fff;
+    height: 100%;
+    padding: 20px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-wrap: wrap;
+    border-radius: 10px;
+`
+const ParceirosContentItem = styled.div`
+    width: 400px;
+    min-width: 270px;
+    height: 300px;
+    padding: 20px;
+    text-align: center;
+    flex-direction: column;
+    font-size: 19px;
+    color: var(--color-descriptions);
+`
+const ParceirosImgItem = styled.img`
+    max-width: 374px;
+    max-height: 120px;
+    @media (max-width: 400px){
+        max-width: 200px
+    }
+
+`
+
 
 export default function Parceiros() {
 
@@ -20,34 +71,35 @@ export default function Parceiros() {
             que ajudam constantemente no crescimento da educação
             e das inovações"
         > 
-            <div className="parceiros">
-                <div className="parceiros-middle">
-                    <div className="parceiros-middle-content">
-                        <div className="parceiros-content-item">
-                            <img alt="" src={UFERSA} className="parceiros-img-item" /><br/>
+            
+            <ParceirosContainer>
+                <ParceirosMiddle>
+                    <ParceirosMiddleContent>
+                        <ParceirosContentItem>
+                        <ParceirosImgItem src={UFERSA} /><br/>
                             <p>Universidade Federal Rural do Semi-árido</p>
-                        </div>
-                        <div className="parceiros-content-item">
-                            <img alt="" src={PROEC} className="parceiros-img-item" /><br/>
+                        </ParceirosContentItem>
+                        <ParceirosContentItem>
+                        <ParceirosImgItem src={PROEC} /><br/>
                             <p>Pró-reitoria de Extensão e Cultura</p>
-                        </div>
-                        <div className="parceiros-content-item">
-                            <img alt="" src={SEBRAE} className="parceiros-img-item" /><br/>
+                        </ParceirosContentItem>
+                        <ParceirosContentItem>
+                        <ParceirosImgItem src={SEBRAE} /><br/>
                             <p>Serviço Brasileiro de Apoio às Micro e Pequenas Empresas</p>
-                        </div>
-                        <div className="parceiros-content-item">
-                            <img alt="" src={ME} className="parceiros-img-item" /><br/>
+                        </ParceirosContentItem>
+                        <ParceirosContentItem>
+                            <ParceirosImgItem src={ME} /><br/>
                             <p>Ministério da Educação - BRASIL</p>
-                        </div>
-                        <div className="parceiros-content-item">
-                            <img alt="" src={RNgov} className="parceiros-img-item" /><br/>
+                        </ParceirosContentItem>
+                        <ParceirosContentItem>
+                        <ParceirosImgItem src={RNgov} /><br/>
                             <p>Governo do Rio Grande do Norte</p>
-                        </div>
-                    </div>
-                </div>
+                        </ParceirosContentItem>
+                    </ParceirosMiddleContent>
+                </ParceirosMiddle>
 
 
-            </div>
+            </ParceirosContainer>
          </PageDefault>
     );
 }
