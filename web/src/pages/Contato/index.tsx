656bfc0cb2c3293e69293ad854fb13cdfd8decac
Emanuel-Bruno <emanuelbruno2018vasc@gmail.com>
2020-10-03 22:34:48 -0300

    Contato/Styled-Components

--- web/src/pages/Contato/index.tsx
+++ web/src/pages/Contato/index.tsx
@@ -10,7 +10,208 @@ import ig from '../../assets/images/Contato/ig.png';
 import tt from '../../assets/images/Contato/tt.png';
 import fb from '../../assets/images/Contato/fb.png';
 import ln from '../../assets/images/Contato/ln.png';
-import './styles.css';
+import styled from 'styled-components';
+
+const Contatos = styled.div`
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+`
+const ContatosPage = styled.div`
+    background: #F0F0F7;
+`
+const ContatosContainer = styled.div`
+    border-radius: 8pt;
+    box-shadow: 0px 7px 4px rgba(0,0,0,0.2);
+    margin: 30px 40px;
+    background: #fff;
+    display: flex;
+    flex-direction: row;
+    font: roboto;
+    padding: 30px 40px;
+    @media (max-width: 400px){
+        margin: 30px 10px;
+        padding: 10px;
+        flex-direction: column;
+    }
+    @media (min-width:401px) and (max-width:900px){
+        margin: 30px 10px;
+        padding: 10px;
+    }
+`
+const ContatoLeft = styled.div`
+    width: 50%;
+    color: var(--color-pagina-textoazulclaro);
+    padding: 30px 50px;
+    flex-direction: column;
+    @media (max-width: 400px){
+        width: 100%;
+        padding: 10px;
+    }
+    @media (min-width:401px) and (max-width:900px){
+        padding: 10px;
+    }
+`
+const ContatoTitulo = styled.h2`
+    @media (max-width: 400px){
+        text-align: center;
+        font-size: 22px;
+    }
+    @media (min-width:401px) and (max-width:900px){
+    font-size: 24px;
+    }
+`
+const ParagraphContainer = styled.p`
+    color: var(--color-pagina-texto);
+    @media (min-width:401px) and (max-width:900px){
+        font-size: 16px;
+    }
+`
+const ContatoInfoTop = styled.div`
+    padding-top: 30px;
+    padding-bottom: 30px;
+    font-size: 14pt;
+    text-align: justify;
+`
+const ContatoLis = styled.div`
+    text-align: justify;
+    font-size: 14pt;
+    @media (max-width: 400px){
+        text-align: center;
+        margin-bottom: 20px;
+    }
+`
+const ParagraphContatoLis = styled.div`
+    text-align: justify;
+    font-size: 10pt;
+    padding: 30px 50% 30px 0;
+    @media (max-width: 400px){
+        padding: 10px 20px;
+    }
+    @media (min-width:401px) and (max-width:900px){
+        padding: 30px 10% 30px 0;
+    }
+
+`
+const EnderecosContato = styled.div`
+    margin: 0 0px 50px 0px;
+    flex-direction: column;
+    @media (max-width: 400px){
+        margin-bottom: 30px;
+    }
+    @media (min-width:401px) and (max-width:900px){
+        margin-bottom: 20px;
+    }
+`
+const EnderecosItem = styled.div`
+    display: flex;
+    align-items: center;
+    margin-right: 19px;
+    margin-bottom: 8px;
+    @media (max-width: 400px){
+        margin-bottom: 10px;
+    }
+`
+const ImageEnderecoContato = styled.img`
+    max-height: 30px;
+`
+const ParagraphEnderecoContato = styled.p`
+font-weight: bold;
+color: var(--color-pagina-textoazul);
+`
+const SocialeContato = styled.div`
+    width: 315px;
+    height: 30;
+    margin: 0 237px 0 84px;
+    display: flex;
+    justify-content: space-between;
+    @media (max-width: 400px){
+        width: 100%;
+        margin: 0;
+        padding: 0 40px;
+    }
+    @media (min-width:401px) and (max-width:900px){
+        margin: 0;
+        padding: 0 40px;
+    }
+`
+const ContatoLine = styled.div`
+    width: 1px;
+    @media (max-width: 400px){
+        width: 100%;
+    }
+`
+const LMid = styled.hr`
+    margin-top: 40px;
+    width: 1px;
+    height: 88%;
+    border: 1px solid #2BB3EB;
+    @media (max-width: 400px){
+        margin-top: 20px;
+        margin-left: 30px;
+        width: 75%;
+        height: 1px;
+    }
+`
+const ContatoRight = styled.div`
+    width: 50%;
+    padding: 40px 50px;
+    @media (max-width: 400px){
+        width: 100%;
+        padding: 30px 20px;
+    }
+    @media (min-width:401px) and (max-width:900px){
+        width: 100%;
+        padding: 10px 20px;
+    }
+`
+const ContatoMsg = styled.div`
+    color: var(--color-pagina-texto);
+    margin-bottom: 50px;
+`
+const GetContatos = styled.div`
+    padding-bottom: 30px;
+`
+const CampoTextoContato = styled.input`
+    width: 100%;
+    height: 56px;
+    border-radius: 8px;
+    border: 1px solid #E6E6F0;
+    padding: auto;
+    font-size: 20px;
+    color: #424242;
+    background-color: #E6E6F0;
+    @media (max-width: 400px){
+        height: 50px;
+    }
+    @media (min-width:401px) and (max-width:900px){
+        height: 50px;
+    }
+`
+const CampoTextoAreaContato = styled.textarea`
+    font-size: 18px;
+    color: #424242;
+    font: roboto;
+    min-width: 100%;
+    max-width: 100%;
+    min-height: 300px;
+    max-height: 300px;
+    border-radius: 8x;
+    border: 1px solid #E6E6F0;
+    background-color: #E6E6F0;
+
+`
+const BtnContato = styled.button`
+    width: 120px;
+    height: 46px;
+    border-radius: 8px;
+    float: right;
+    margin-top: 20px;
+    border: none;
+    font-weight: bold;
+    color: var(--color-botao-texto);
+    background-color: var(--color-pagina-botao);
+`
 
 export default function Contato() {
 
@@ -41,14 +242,13 @@ export default function Contato() {
             title="Vamos nos conhecer um pouco mais"
             description="Deixe seu feedback para melhorarmos cada vez mais!"
         >
-            <div className="contato">
-                
-                <div className="contato-page">
-                    <div className="contato-container">
-                        <div className="contato-left">
-                            <div className="contato-infoTop">
-                                <h1 className="contato-title">Entre em contato conosco!</h1>
-                                <p>
+            <Contatos>
+                <ContatosPage>
+                    <ContatosContainer>
+                        <ContatoLeft>
+                            <ContatoInfoTop>
+                                <ContatoTitulo>Entre em contato conosco!</ContatoTitulo>
+                                <ParagraphContainer>
                                     Id cursus metus aliquam eleifend mi in nulla posuere
                                     sollicitudin. Ut pharetra  sit amet aliquam id diam maecenas
                                     ultricies. Euismod quis viverra nibh cras pulvinar mattis.
@@ -56,77 +256,77 @@ export default function Contato() {
                                     ullamcorentesque adipiscing commodo  elit at imperdiet.
                                     Orci sagittis eu volutpat odio facilisis mauris sit amet
                                     massa.
-                                </p>
-                            </div>
-                            <div className="contato-lis">
+                                </ParagraphContainer>
+                            </ContatoInfoTop>
+                            <ContatoLis>
                                 <h3>LIS</h3>
-                                <p>
+                                <ParagraphContatoLis>
                                     Id cursus metus aliquam eleifend mi in
                                     nulla posuere sollicitudin. Ut pharetra sit
                                     amet aliquam id diam maecenas ultricies.
                                     Euismod quis viverra nibh cras pulvinar
                                     mattis. Amet nulla facilisi morbi tempus
                                     iaculis. Vestibulum mattis ullamcorentesque
-                                </p>
-                            </div>
-                            <div className="contatos">
-                                <div className="contatos-item">
+                                </ParagraphContatoLis>
+                            </ContatoLis>
+                            <EnderecosContato>
+                                <EnderecosItem>
                                     <div>
-                                        <img src={group} alt="" />
+                                        <ImageEnderecoContato src={group} alt="" />
                                     </div>
                                     <div>
-                                        <p>sitelisexample.com.br</p>
+                                        <ParagraphEnderecoContato>sitelisexample.com.br</ParagraphEnderecoContato>
                                     </div>
-                                </div>
-                                <div className="contatos-item">
+                                </EnderecosItem>
+                                <EnderecosItem>
                                     <div>
-                                        <img src={mail} alt="" />
-                                    </div>
-                                    <div>
-                                        <p>sitelisexample@.com.br</p>
-                                    </div>
-                                </div>
-                                <div className="contatos-item">
-                                    <div>
-                                        <img src={local} alt="" />
+                                        <ImageEnderecoContato src={mail} alt="" />
                                     </div>
                                     <div>
-                                        <p>Endereço: xxxxxx-xxxxx, 00000000</p>
+                                        <ParagraphEnderecoContato>sitelisexample@.com.br</ParagraphEnderecoContato>
                                     </div>
-                                </div>
-                            </div>
-                            <div className="sociale">
+                                </EnderecosItem>
+                                <EnderecosItem>
+                                    <div>
+                                        <ImageEnderecoContato src={local} alt="" />
+                                    </div>
+                                    <div>
+                                        <ParagraphEnderecoContato>Endereço: xxxxxx-xxxxx, 00000000</ParagraphEnderecoContato>
+                                    </div>
+                                </EnderecosItem>
+                            </EnderecosContato>
+                            <SocialeContato>
                                 <Link to=""><img src={ig} alt="" /></Link>
                                 <Link to=""><img src={tt} alt="" /></Link>
                                 <Link to=""><img src={fb} alt="" /></Link>
                                 <Link to=""><img src={ln} alt="" /></Link>
-                            </div>
-                        </div>
-                        <div className="contato-line">
-                            <hr className="lmid" />
-                        </div>
-                        <div className="contato-right">
-                            <div className="contato-msg">
+                            </SocialeContato>
+                        </ContatoLeft>
+                        <ContatoLine>
+                            <LMid />
+                        </ContatoLine>
+                        <ContatoRight>
+                            <ContatoMsg>
                                 <h2>Ou envie uma mensagem</h2>    
-                            </div>
-                            <div className="get-contatos">
+                            </ContatoMsg>
+                            <GetContatos>
                                 <p>Nome</p>
-                                <input type="text" />
-                            </div>
-                            <div className="get-contatos">
+                                <CampoTextoContato type="text" />
+                            </GetContatos>
+                            <GetContatos>
                                 <p>Email</p>
-                                <input type="text" />
-                            </div>
-                            <div className="get-contatos">
+                                <CampoTextoContato type="text" />
+                            </GetContatos>
+                            <GetContatos>
                                 <p>Bio (max. 300 caracteres)</p>
-                                <textarea maxLength={300}></textarea>
-                                <button className="btn-e">Enviar</button>
-                            </div>
-                        </div>
-                    </div>
-
-                </div>
-            </div>
+                                <CampoTextoAreaContato maxLength={300}></CampoTextoAreaContato>
+                                <BtnContato> Enviar
+                                </BtnContato>
+                            </GetContatos>
+                        </ContatoRight>
+                    </ContatosContainer>
+                </ContatosPage>
+            </Contatos>
         </PageDefault>
     );
 }
